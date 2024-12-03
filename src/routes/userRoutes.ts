d35bbe85d27d75fda79dd260719b867f4eda469a
josup <alexlunabares@gmail.com>
2024-12-03 12:54:17 +0100

    added the middleware to the userRouter to protect routes

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,10 +1,14 @@
 import { Router } from "express";
 import { registerUser, loginUser } from "../controllers/userController";
+import { deserializeUser } from "../middlewares/deserializeUser";
 
 const userRouter = Router();
 
 userRouter.post("/user/register", registerUser);
 userRouter.post("/user/login", loginUser);
 //userRouter.post("/user/logout", logoutUser)
+
+//Protected routes
+userRouter.use(deserializeUser);
 
 export { userRouter };

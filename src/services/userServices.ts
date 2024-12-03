fb7a51e6f2878996f769d6272757bfb4784c1701
josup <alexlunabares@gmail.com>
2024-12-03 12:51:12 +0100

    added jwt auth utils

--- src/services/userServices.ts
+++ src/services/userServices.ts
@@ -4,44 +4,45 @@ import { create, findByEmail } from "../db/userRepository";
 import { CustomError } from "../utils/customError";
 
 const addUserSchema = z.object({
-  name: z.string(),
-  email: z.string().email(),
-  password: z.string().min(8),
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(8),
 });
 
 const loginoutUserSchema = z.object({
-  email: z.string().email(),
-  password: z.string().min(8),
+    email: z.string().email(),
+    password: z.string().min(8),
 });
 
 // register
 export class UserService {
-  static async addUser(name: string, email: string, password: string) {
-    addUserSchema.parse({ name, email, password });
-    //check in db if email already exists
-    if (await findByEmail(email)) {
-      throw new CustomError("Email is already registered.", 409);
+    static async addUser(name: string, email: string, password: string) {
+        addUserSchema.parse({ name, email, password });
+        //check in db if email already exists
+        if (await findByEmail(email)) {
+            throw new CustomError("Email is already registered.", 409);
+        }
+
+        // hash password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        // save user in db
+        const user = create(email, hashedPassword, name);
+        return user;
     }
 
-    // hash password
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
+    static async loginUser(email: string, password: string) {
+        loginoutUserSchema.parse({ email, password });
+        const user = await findByEmail(email);
+        if (!user) {
+            throw new CustomError("Email is not registered.", 404);
+        }
+        if (!(await bcrypt.compare(password, user.password))) {
+            throw new CustomError("Wrong email or password.", 401);
+        }
 
-    // save user in db
-    const user = create(email, hashedPassword, name);
-    return user;
-  }
-
-  static async loginUser(email: string, password: string) {
-    loginoutUserSchema.parse({ email, password });
-    const user = await findByEmail(email);
-    if (!user) {
-      throw new CustomError("Email is not registered.", 404);
+        console.log(`User ${user.email} logged in`);
+        return user;
     }
-    if (!(await bcrypt.compare(password, user.password))) {
-      throw new CustomError("Wrong email or password.", 401);
-    }
-    // TODO: return user with token
-    return console.log("logged in");
-  }
 }

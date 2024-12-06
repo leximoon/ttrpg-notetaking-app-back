38396df9481e03ab2206b3c6c714feb895bdce0b
josup <alexlunabares@gmail.com>
2024-12-06 21:23:09 +0100

    added auth middleware

--- src/routes/documentRoutes.ts
+++ src/routes/documentRoutes.ts
@@ -1,14 +1,15 @@
 import { Router } from "express";
-import { deserializeUser } from "../middlewares/deserializeUser";
+
 import {
-  createDocument,
-  loadUserDocuments,
+    createDocument,
+    loadUserDocuments,
 } from "../controllers/documentController";
+import { isAuthenticated } from "../middlewares/isAuthenticated";
 
 const documentRouter = Router();
 
 //Protected routes
-//documentRouter.use(deserializeUser);
+documentRouter.use(isAuthenticated);
 
 documentRouter.post("/add", createDocument);
 documentRouter.get("/load", loadUserDocuments);

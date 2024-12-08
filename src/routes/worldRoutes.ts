fb8e14a0ece2df82caae607116ddc371b3bd89a2
josup <alexlunabares@gmail.com>
2024-12-08 20:07:42 +0100

    Modified names to clarify functionality Modified routing to clarify functionality Added new route to get a world added controller, service and repository function to handle new route

--- src/routes/worldRoutes.ts
+++ src/routes/worldRoutes.ts
@@ -3,18 +3,22 @@ import { Router } from "express";
 import { isAuthenticated } from "../middlewares/isAuthenticated";
 import {
     createWorld,
-    currentUserWorlds,
-    getAllWorlds,
+    getCurrentUserWorlds,
+    getAllPublicWorlds,
+    getWorld,
 } from "../controllers/worldController";
 
 const worldRouter = Router();
 
 //Get all public worlds
-worldRouter.get("/", getAllWorlds);
+worldRouter.get("/public", getAllPublicWorlds);
 
 worldRouter.use(isAuthenticated);
 
 worldRouter.post("/", createWorld);
-worldRouter.get("/me", currentUserWorlds);
+
+worldRouter.get("/me", getCurrentUserWorlds);
+
+worldRouter.get("/:worldId", getWorld);
 
 export { worldRouter };
